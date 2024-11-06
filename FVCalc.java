0bd0a9dbdb621b158b728fd095023671f99c2b34
Yarin Kovalski <yarin.kovalski@post.runi.ac.il>
2024-11-06 21:31:07 +0200

    FVCalc second try

--- FVCalc.java
+++ FVCalc.java
@@ -11,12 +11,13 @@ public class FVCalc
         Scanner sc= new Scanner(System.in); 
 		// Replace this comment with your code
         int currentValue = Integer.parseInt(args[0]);
-        int rate = Integer.parseInt(args[1]);
+        double rate = Double.parseDouble(args[1]);
         int n = Integer.parseInt(args[2]);
-        double RaseXToY= 0;
+        double futureValue= 0;
         double val = Double.valueOf(rate);
-        RaseXToY = Math.pow(val+1, n);
-        System.out.println("AAfter " + n + " years, a $" + currentValue+ " saved at " + val+"%25 will yield " + "$"+((int)RaseXToY));
+		System.out.println(currentValue+" "+rate+" "+n);
+        futureValue = currentValue * (Math.pow(((rate/100)+1), n));
+        System.out.println("After " + n + " years, a $" + currentValue+ " saved at " + val+"%25 will yield " + "$"+((int)futureValue));
 		//AAfter 2 years, a $100 saved at 10.0%25 will yield $121
 
 
