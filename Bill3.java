98a29a43a718ab8634338148faee41f70d090c7f
Yarin Kovalski <yarin.kovalski@post.runi.ac.il>
2024-11-07 08:21:49 +0200

    Bill First try

--- Bill3.java
+++ Bill3.java
@@ -1,25 +1,18 @@
-import java.util.*;  
-
-
-
-// Splits a restaurant bill evenly among three diners.
+// Exe1 Splits a restaurant bill evenly among three diners.
 public class Bill3 
 {
 
     	public static void main(String[] args) 
     {
 
-        Scanner sc= new Scanner(System.in); 
-		// To get you started, here is the first line in the program:
-	    String name1 = args[0];
-        String name2 = args[1];
-        String name3 = args[2];
+	    String Name1 = args[0];  //First name
+        String Name2 = args[1];  //Second name
+        String Name3 = args[2];	 //Third name
         int bill = Integer.parseInt(args[3]);
-        double division = (double) bill/3; //the bill/3 (3 people)
-        division = Math.ceil(division); //round up
-        System.out.println("Dear "+ name3 + ", " + name2+ ", and " + name1 + ": pay " + division + " Shekels each");
-	    // Replace this comment with the rest of your code   
-		// Dear a, b, and c: pay 80.0 each.
+        double Division = (double) bill/3; //The bill/3 (3 people)
+        Division = Math.ceil(Division); //Round up variables
+        System.out.println("Dear "+ Name3 + ", " + Name2+ ", and " + Name1 + ": pay " + Division + " Shekels each");
+
 	
 	}
 }
